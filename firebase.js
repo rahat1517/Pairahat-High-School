b8ead63339804a90c17787fec6c1c858c9aad3bb
rahat1517 <157340060+rahat1517@users.noreply.github.com>
2026-02-11 21:49:43 +0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz79R-4Kvo5ImobTbda27K4YRpaMlxtuc",
+  authDomain: "public-result-system.firebaseapp.com",
+  projectId: "public-result-system",
+  storageBucket: "public-result-system.appspot.com",
+  messagingSenderId: "326336877848",
+  appId: "1:326336877848:web:b06f81b8be6c23f3c0ba6d"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
